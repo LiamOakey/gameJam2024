3bda7062e0fad223d6f1185550ebbe37ca803102
Liam Oakey <lm411824@dal.ca>
2024-01-28 11:56:43 -0400

    added map border and made enemies not spawn outside it

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -17,13 +17,23 @@ public class EnemySpawner : MonoBehaviour
     void continuiousSpawn(GameObject enemy){
         spawnTimer-=Time.deltaTime;
         if(spawnTimer < 0){
-            spawnEnemy(enemy);
-            spawnTimer = spawnDelay;
+            bool spawned = spawnEnemy(enemy);
+
+            if(spawned == true) spawnTimer = spawnDelay;
         }
     }
-    //EnemySpawner will spawn at a random spawnpoint under the variable spawnPoint
-    void spawnEnemy(GameObject enemy){
+
+    //EnemySpawner will spawn at a random spawnpoint under the variable spawnPoint, it will return true if the enemy is spawned in
+    bool spawnEnemy(GameObject enemy){
         int spawnEnemyNum = (int) Random.Range(0,spawnPoint.transform.childCount-1);
+
+        //Ensure enemies are inside the map
+        float x = spawnPoint.transform.GetChild(spawnEnemyNum).transform.position.x;
+        float y = spawnPoint.transform.GetChild(spawnEnemyNum).transform.position.y;
+        if(x < -25 || x > 25 || y > 19 || y < -20) return false;
+
+
         Instantiate(enemy, spawnPoint.transform.GetChild(spawnEnemyNum).transform.position, Quaternion.identity);
+        return true;
     }
 }
